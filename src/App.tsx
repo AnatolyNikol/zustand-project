9b23a5502965b8fb74ca2f348e084cf054edd769
Anatoly Nikolenko <nikolenko_av@sima-land.ru>
2025-02-27 09:45:12 +0500

    add persist

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,31 @@ import './App.css';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import { useCoffeeStore } from './model/coffeeStore';
 import { useEffect, useState } from 'react';
+import { useCounterStore } from './model/counter.store';
 
 function App() {
-    const { getCoffeeList, coffeeList } = useCoffeeStore();
-    const [text, setText] = useState<string>('');
+    // const { getCoffeeList, coffeeList } = useCoffeeStore();
+    // const [text, setText] = useState<string>('');
 
-    const coffeeSearchHandler = (text: string) => {
-        getCoffeeList({ text });
-        setText(text);
-    };
+    // const coffeeSearchHandler = (text: string) => {
+    //     getCoffeeList({ text });
+    //     setText(text);
+    // };
 
-    useEffect(() => {
-        getCoffeeList();
-    }, []);
+    // useEffect(() => {
+    //     getCoffeeList();
+    // }, []);
+
+    const { counter, decrement, increment, persistedCounter } =
+        useCounterStore();
 
     return (
         <div className='wrapper'>
-            <Input
+            <Button onClick={increment}>+</Button>
+            <span>{counter}</span>
+            <span>{persistedCounter}</span>
+            <Button onClick={decrement}>-</Button>
+            {/* <Input
                 placeholder='поиск'
                 value={text}
                 onChange={(e) => coffeeSearchHandler(e.currentTarget.value)}
@@ -50,7 +58,7 @@ function App() {
                             />
                         </Card>
                     ))}
-            </div>
+            </div> */}
         </div>
     );
 }
